828ba34fce845947150b4753134b97c3d9c6bb70
paol <64162487+Paol-M@users.noreply.github.com>
2022-12-30 22:36:38 +0000

    Mainly, zoom-in & -out

---
+++ script.js
@@ -0,0 +1,68 @@
+console.log("%c🔒️ SimpleType is private\nContribute on GitHub.", "background-color:#cfbcff;color:#3a0092;font-size:4vw;font-family:system-ui;");
+
+const info = document.getElementById("info");
+const type = document.getElementById("type");
+const minus = document.getElementById("-");
+const plus = document.getElementById("+");
+
+info.checked = false;
+type.focus();
+
+document.getElementById("-+").style.display ="flex";
+
+type.onclick = closeMenu;
+function closeMenu() {
+  info.checked = false;
+}
+
+document.addEventListener("keyup", (evt) => {
+  if (evt.key == "Escape") {
+    info.checked = false;
+    type.focus();
+  }
+});
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js");
+}
+
+minus.onclick = minusSize;
+function minusSize() {
+  var x = window.getComputedStyle(type).fontSize;
+  var y = x.replace("px", "");
+  let z = +y;
+
+  if (z > 10) {
+    type.style.fontSize = y * 0.9 + "px";
+
+    plus.style.fillOpacity = 1;
+    plus.style.cursor = "pointer";
+    
+    minus.style.fillOpacity = 1;
+    minus.style.cursor = "pointer";
+  } else {
+    minus.style.fillOpacity = 0.5;
+    minus.style.cursor = "not-allowed";
+  }
+}
+
+plus.onclick = addSize;
+function addSize() {
+  var x = window.getComputedStyle(type).fontSize;
+  var y = x.replace("px", "");
+  let z = +y;
+  console.log(isFinite(z));
+
+  if (z < 200) {
+    type.style.fontSize = z * 1.1 + "px";
+
+    plus.style.fillOpacity = 1;
+    plus.style.cursor = "pointer";
+
+    minus.style.fillOpacity = 1;
+    minus.style.cursor = "pointer";
+  } else {
+    plus.style.fillOpacity = 0.5;
+    plus.style.cursor = "not-allowed";
+  }
+}
--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
-const cName="demo-pwa",cFiles=["index.html","/css/style.css"];self.addEventListener("install",e=>{e.waitUntil(caches.open(cName).then(e=>e.addAll(cFiles)).catch(e=>{console.error(e)}))}),self.addEventListener("fetch",s=>{s.respondWith(caches.match(s.request).then(e=>e||fetch(s.request)))});
+const cName = "demo-pwa",
+cFiles = [
+  "index.html",
+  "/css/style.css"
+];
+
+// (B) CREATE/INSTALL CACHE
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(cName)
+    .then((cache) => { return cache.addAll(cFiles); })
+    .catch((err) => { console.error(err) })
+  );
+});
+ 
+// (C) CACHE STRATEGY
+self.addEventListener("fetch", (evt) => {
+  // (C1) LOAD FROM CACHE FIRST, FALLBACK TO NETWORK IF NOT FOUND
+  evt.respondWith(
+    caches.match(evt.request)
+    .then((res) => { return res || fetch(evt.request); })
+  );
+
+});
