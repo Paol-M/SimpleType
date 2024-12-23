2d07bbf6835b018d238872ec35442ee149313312
Paol-M <paolomassoud@gmail.com>
2024-12-23 04:56:19 +0200

    Hot fixed counter + small improvements. The app is temporarily no longer a PWA.

---
+++ js/wordCount.js
@@ -0,0 +1,49 @@
+import { countWords, countLines } from "alfaaz";
+
+const wCount = document.getElementById("wCount");
+const sCount = document.getElementById("sCount");
+const count = document.getElementById("count");
+const type = document.getElementById("type");
+const clearText = document.getElementById('clearText');
+
+let lineMode = false;
+
+
+count.style.display = "flex";
+
+count.onclick = function () {
+  if (lineMode) {
+    lineMode = false;
+    sCount.style.display = "none";
+    wCount.style.display = "flex";
+  } else {
+    lineMode = true;
+    wCount.style.display = "none";
+    sCount.style.display = "flex";
+  }
+  updateCount();
+}
+
+function updateCount() {
+  let textContent = type.value;
+  let count = lineMode ? countLines(textContent) : countWords(textContent);
+  if (lineMode) {
+    sCount.textContent = count.toLocaleString() + " line" + (count === 1 ? "" : "s");
+  } else {
+    wCount.textContent = count.toLocaleString() + " word" + (count === 1 ? "" : "s");
+  }
+}
+
+export function countSelection() {
+  let selection = window.getSelection();
+  let textContent = selection.toString();
+  let count = lineMode ? countLines(textContent) : countWords(textContent);
+  if (lineMode) {
+    sCount.textContent = count.toLocaleString() + " line" + (count === 1 ? "" : "s");
+  } else {
+    wCount.textContent = count.toLocaleString() + " word" + (count === 1 ? "" : "s");
+  }
+}
+
+type.addEventListener("input", updateCount);
+clearText.addEventListener("click", updateCount);
